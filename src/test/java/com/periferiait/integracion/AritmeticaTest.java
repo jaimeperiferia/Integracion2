be8806e02c3d2061086d08c691193ca2042e12de
jaimeperiferia <jaimebonilla@cbit-online.com>
2019-03-29 16:40:07 -0500

    java 8

--- src/test/java/com/periferiait/integracion/AritmeticaTest.java
+++ src/test/java/com/periferiait/integracion/AritmeticaTest.java
@@ -4,12 +4,11 @@
  * and open the template in the editor.
  */
 package com.periferiait.integracion;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+
+
 
 /**
  *
@@ -20,21 +19,6 @@ public class AritmeticaTest {
     public AritmeticaTest() {
     }
     
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
     
     @Test
     public void suma() {
